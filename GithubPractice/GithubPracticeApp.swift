d997acc44ad52ab3045e55f9d7839ad4759360b6
Jason Bartley <83716676+jasonbartley3990@users.noreply.github.com>
2024-10-08 20:17:44 -0400

    Initial Commit

---
+++ GithubPractice/GithubPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubPracticeApp.swift
+//  GithubPractice
+//
+//  Created by Jason bartley on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
